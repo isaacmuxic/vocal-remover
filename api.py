15a5ccca48b0a146e00131725cf2237defb8d1f5
isaacmuxic <isaac.chong@muxic.io>
2023-04-04 12:01:42 +0800

    fix post form file

--- api.py
+++ api.py
@@ -13,44 +13,41 @@ from lib import nets
 from lib import spec_utils
 from lib import utils
 # web service variation
-from flask import Flask
+from flask import Flask, request, send_file
 from flask_restful import Resource, Api, reqparse
+from flask_cors import CORS
 
 # any name will work for internal identification
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 class remover(Resource):
     # methods go here
     def post(self):
-        p = reqparse.RequestParser()  # initialize
-        p.add_argument('gpu', type=int, default=-1)
-        p.add_argument('pretrained_model', type=str, default='models/baseline.pth')
-        p.add_argument('input', required=True)
-        p.add_argument('sr', type=int, default=44100)
-        p.add_argument('n_fft', type=int, default=2048)
-        p.add_argument('hop_length', type=int, default=1024)
-        p.add_argument('batchsize', type=int, default=4)
-        p.add_argument('cropsize', type=int, default=256)
-        p.add_argument('output_image', action='store_true')
-        p.add_argument('postprocess', action='store_true')
-        p.add_argument('tta', action='store_true')
-        p.add_argument('output_dir', type=str, default="/download")
-        args = p.parse_args()
+        args_gpu = -1
+        args_pretrained_model = 'models/baseline.pth'
+        args_sr = None
+        args_n_fft = 2048
+        args_hop_length = 1024
+        args_batchsize = 4
+        args_cropsize = 256
+        args_tta = False
+        args_postprocess = False
+        args_output_dir = "download"
 
-        filename = random_file(prefix="temp",extension="mp3")
-        filepath = os.path.join(args.output_dir, filename)
-        with open(filepath, "wb") as temp:
-            temp.write(args.input)
-            temp.close
+        temp = request.files['input']
+        filename = temp.filename # random_file(prefix="temp",extension="mp3")
+        filepath = os.path.join(args_output_dir, filename)
+        temp.save(filepath)
         
         print('loading model...', end=' ')
         device = torch.device('cpu')
-        model = nets.CascadedNet(args.n_fft, 32, 128)
-        model.load_state_dict(torch.load(args.pretrained_model, map_location=device))
-        if args.gpu >= 0:
+        model = nets.CascadedNet(args_n_fft, 32, 128)
+        model.load_state_dict(torch.load(args_pretrained_model, map_location=device))
+        if args_gpu >= 0:
             if torch.cuda.is_available():
-                device = torch.device('cuda:{}'.format(args.gpu))
+                device = torch.device('cuda:{}'.format(args_gpu))
                 model.to(device)
             elif torch.backends.mps.is_available() and torch.backends.mps.is_built():
                 device = torch.device('mps')
@@ -59,7 +56,7 @@ class remover(Resource):
 
         print('loading wave source...', end=' ')
         X, sr = librosa.load(
-            filepath, args.sr, False, dtype=np.float32, res_type='kaiser_fast')
+            filepath, args_sr, False, dtype=np.float32, res_type='kaiser_fast')
         # basename = os.path.splitext(os.path.basename(args.input))[0]
         print('done')
 
@@ -68,12 +65,12 @@ class remover(Resource):
             X = np.asarray([X, X])
 
         print('stft of wave source...', end=' ')
-        X_spec = spec_utils.wave_to_spectrogram(X, args.hop_length, args.n_fft)
+        X_spec = spec_utils.wave_to_spectrogram(X, args_hop_length, args_n_fft)
         print('done')
 
-        sp = Separator(model, device, args.batchsize, args.cropsize, args.postprocess)
+        sp = Separator(model, device, args_batchsize, args_cropsize, args_postprocess)
 
-        if args.tta:
+        if args_tta:
             y_spec, v_spec = sp.separate_tta(X_spec)
         else:
             y_spec, v_spec = sp.separate(X_spec)
@@ -91,18 +88,20 @@ class remover(Resource):
         # sf.write('{}{}_Instruments.wav'.format(output_dir, basename), wave.T, sr)
 
         print('inverse stft of vocals...', end=' ')
-        wave = spec_utils.spectrogram_to_wave(v_spec, hop_length=args.hop_length)
+        wave = spec_utils.spectrogram_to_wave(v_spec, hop_length=args_hop_length)
         print('done')
         filename = 'vocal_' + filename
-        filepath = os.path.join(args.output_dir, filename)
+        filepath = os.path.join(args_output_dir, filename)
         with AudioFile(filepath, "w", samplerate=sr, num_channels=2, quality=128,  # kilobits per second
         ) as f:
             f.write(wave)
         # sf.write(filepath, wave.T, sr)
-        temp = open(filepath, "rb")
-        result = temp.read()
-        temp.close
-        return {'vocal': result}, 200  # return data with 200 OK 
+
+        return send_file(filepath)
+        # temp = open(filepath, "rb")
+        # result = temp.read()
+        # temp.close
+        # return {'vocal': result}, 200  # return data with 200 OK 
     pass
 
 api.add_resource(remover, '/remover') 
